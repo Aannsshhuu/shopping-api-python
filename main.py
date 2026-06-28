dec3e7180bba4bfcdb56e409b7b422588a670619
Aannsshhuu <76780259+Aannsshhuu@users.noreply.github.com>
2026-06-28 19:21:45 +0530

    Connected product router

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from routers.products import router as productRouter
 
 app = FastAPI()
+
+app.include_router(
+    productRouter,
+    prefix="/products",
+    tags=["Products"]
+)
 
 @app.get("/")
 def home():
